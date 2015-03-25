dac0f06f12d14c4bc615f4743c952f54b0bddfe4
Erik <Bernhardsson@Eriks-MacBook-Air-2.local>
2015-03-25 13:42:45 -0400

    Object oriented stuff

--- demo.js
+++ demo.js
@@ -3,69 +3,124 @@ var model = RandomForest.deserialize(data);
 var app = angular.module("myApp",[]);
 app.controller('InputController', function($scope){
 
-  var row = [];
-  $scope.samples = [];
-  for (var j = 0; j < headers.length; j++) {
-    row.push(undefined);
-    $scope.samples.push([]);
-  }
-  
-  for (var i = 0; i < 1000; i++) {
-    var sample = model.fill(row);
-    for (var j = 0; j < row.length; j++)
-      $scope.samples[j].push(sample[j]);
-  }
+  $scope.row = [];
+  for (var j = 0; j < headers.length; j++)
+    $scope.row.push(undefined);
 
   $scope.headers = headers.map(function(x) { return x.replace(/_/g, ' '); });
+  $scope.charts = [];
 });
 
 app.directive("renderChart",function(){
-  return function(scope, element, attrs){
-    var index = scope.$index;
-    histogram(element, scope.samples[index]);
+  return function($scope, element, attrs){
+    var chart = new Chart(element);
+    $scope.charts.push(chart);
+    
+    if ($scope.charts.length == $scope.row.length) {
+      redraw($scope.charts, $scope.row);
+    }
   }
-})
+});
 
-function histogram(element, samples) {
-  var margin = {top: 10, right: 30, bottom: 30, left: 30},
-      width = element[0].offsetWidth,
-      height = element[0].offsetHeight;
+function redraw(charts, row) {
+  var samples = [];
+  for (var j = 0; j < row.length; j++)
+    samples.push([]);
+
+  for (var i = 0; i < 1000; i++) {
+    var sample = model.fill(row);
+    for (var j = 0; j < row.length; j++)
+      samples[j].push(sample[j]);
+  }
+
+  for (var j = 0; j < row.length; j++) {
+    console.log('redrawing ' + j);
+    charts[j].update(samples[j]);
+  }
+}
+
+function Chart(element) {
+  this.margin = {top: 10, right: 30, bottom: 30, left: 30};
+  this.width = element[0].offsetWidth,
+  this.height = element[0].offsetHeight,
+  this.nBins = 30;
+
+  var barWidth = Math.floor(this.width/this.nBins);
+  this.width = barWidth * this.nBins;
   
-  var x = d3.scale.linear()
+  var svg = d3.select(element[0])
+      .append('svg')
+      .attr("width", this.width + this.margin.left + this.margin.right)
+      .attr("height", this.height + this.margin.top + this.margin.bottom)
+      .append("g");
+
+/*  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + (height-margin.bottom) + ")")
+    .call(xAxis);*/
+
+  this.path = svg.append('path')
+    .attr('stroke', 'black')
+    .attr('stroke-weight', '5')
+    .attr('fill', 'none');
+
+  /*var focus = svg.append("g")
+      .attr("class", "focus")
+      .style("display", "none");
+  
+  focus.append("circle")
+    .attr("r", 4.5);
+  
+  focus.append("text")
+    .attr("x", 9)
+    .attr("dy", ".35em");
+
+  svg.append("rect")
+    .attr("fill", "none")
+    .attr("pointer-events", "all")
+    .attr("width", width)
+    .attr("height", height)
+    .on("mouseover", function() { focus.style("display", null); })
+    .on("mouseout", function() { focus.style("display", "none"); })
+    .on("mousemove", mousemove);
+
+
+  this.bisectData = d3.bisector(function(d) { return d.x; }).left;
+  
+  function mousemove() {
+    var x0 = x.invert(d3.mouse(this)[0]),
+	i = bisectData(data, x0, 1),
+	d = data[i];
+    // TODO: linear interpolateation 
+    focus.attr("transform", "translate(" + x(d.x) + "," + y(d.y) + ")");
+    focus.select("text").text(d.x);
+    setValue(d.x);
+  }*/
+}
+
+Chart.prototype.update = function(samples) {
+ var x = d3.scale.linear()
       .domain([d3.min(samples), d3.max(samples)])
-      .range([0, width]);
+      .range([0, this.width]);
   
-  var data = d3.layout.histogram()
-      .bins(x.ticks(40))
-  (samples);
-  
+  var bins = d3.layout.histogram()
+      .bins(x.ticks(this.nBins));
+
+  var data = bins(samples);
+
   var y = d3.scale.linear()
       .domain([0, d3.max(data, function(d) { return d.y; })])
-      .range([height, 0]);
-  
+      .range([this.height, 0]);
+
   var xAxis = d3.svg.axis()
       .scale(x)
       .orient("bottom");
 
-  var svg = d3.select(element[0])
-      .append('svg')
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g");
+  var lineFunction = d3.svg.line()
+      .x(function(d) { return x(d.x); })
+      .y(function(d) { return y(d.y); })
+      .interpolate("basis");
 
-  var bar = svg.selectAll(".bar")
-      .data(data)
-      .enter().append("g")
-      .attr("class", "bar")
-      .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
-  
-  bar.append("rect")
-    .attr("x", 1)
-    .attr("width", x(data[0].dx) - 1)
-    .attr("height", function(d) { return height - y(d.y); });
-  
-  svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
+  this.path.attr('d', lineFunction(data))
 }
+
