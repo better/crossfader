7dd1f21af6504c2698fbbbdd304535570e144f40
Erik Bernhardsson <erikbern@spotify.com>
2015-03-29 12:41:15 -0400

    remove dimensions and predict them

--- autoencoder.py
+++ autoencoder.py
@@ -21,28 +21,40 @@ for i in xrange(K):
     j = random.randint(0, len(headers)-1)
     splits.append((j, random.choice(values[j])))
 
-def get_row(data_row):
-    M_row = numpy.zeros(K)
-    V_row = numpy.zeros(K)
+def get_row(data_row, headers_keep=None):
+    # V: values
+    V_row = numpy.zeros(K, dtype=theano.config.floatX)
+    # M: what values are missing
+    M_row = numpy.zeros(K, dtype=theano.config.floatX)
+    # Q: what values to predict
+    Q_row = numpy.zeros(K, dtype=theano.config.floatX)
 
     for k, split in enumerate(splits):
         j, x_split = split
         if headers[j] not in data_row:
+            M_row[k] = 1
             continue
         x = data_row[headers[j]]
         if x < x_split:
-            M_row[k] = 1
-        elif x > x_split:
-            M_row[k] = 1
             V_row[k] = 1
 
-    return M_row, V_row
+        if headers_keep is not None and headers[j] not in headers_keep:
+            Q_row[k] = 1
 
-M = numpy.zeros((D, K), dtype=theano.config.floatX)
-V = numpy.zeros((D, K), dtype=theano.config.floatX)
+    return V_row, M_row, Q_row
 
-for i, key in enumerate(data.keys()):
-    M[i], V[i] = get_row(data[key])
+def build_matrices():
+    V = numpy.zeros((D, K), dtype=theano.config.floatX)
+    M = numpy.zeros((D, K), dtype=theano.config.floatX)
+    Q = numpy.zeros((D, K), dtype=theano.config.floatX)
+
+    for i, key in enumerate(data.keys()):
+        # How many header should we remove
+        n_headers_keep = random.randint(0, len(headers))
+        headers_keep = set(random.sample(headers, n_headers_keep))
+        V[i], M[i], Q[i] = get_row(data[key], headers_keep)
+
+    return V, M, Q
 
 def W_values(n_in, n_out):
     return numpy.random.uniform(
@@ -54,7 +66,7 @@ srng = theano.tensor.shared_randomstreams.RandomStreams()
 
 def get_parameters():
     # Train an autoencoder to reconstruct the rows of the V matrices
-    n_hidden_layers = 2
+    n_hidden_layers = 3
     n_hidden_units = 64
     Ws, bs = [], []
     for l in xrange(n_hidden_layers + 1):
@@ -70,30 +82,30 @@ def get_parameters():
 
     return Ws, bs
 
-def get_model(Ws, bs):
-    m = T.matrix('mask')
+def get_model(Ws, bs, dropout=False):
     v = T.matrix('input')
+    m = T.matrix('missing')
+    q = T.matrix('target')
 
-    # Dropout in the input as well
-    # use_input_dropout = T.scalar()
-    m_dropped = m * srng.binomial(n=1, p=0.5, size=v.shape)
-    h = v * m_dropped + 0.5 * (1 - m_dropped) # set all unknkown values to 0.5
+    # Set all missing/target values to 0.5
+    keep_mask = (1-m) * (1-q)
+    h = v * keep_mask + 0.5 * (1 - keep_mask)
     
     for l in xrange(len(Ws)):
-
         h = T.dot(h, Ws[l]) + bs[l]
 
         if l < len(Ws) - 1:
             h = h * (h > 0) # relu
-            mask = srng.binomial(n=1, p=0.5, size=h.shape)
-            h = h * mask * 2
+            if dropout:
+                mask = srng.binomial(n=1, p=0.5, size=h.shape)
+                h = h * mask * 2
 
     output = sigmoid(h)
     
     LL = v * T.log(output) + (1 - v) * T.log(1 - output)
-    loss = -(m * LL).sum() / m.sum()
+    loss = -(q * LL).sum() / q.sum()
 
-    return m, v, loss
+    return v, m, q, output, loss
 
 def nesterov_updates(loss, all_params, learn_rate, momentum):
     updates = []
@@ -107,13 +119,22 @@ def nesterov_updates(loss, all_params, learn_rate, momentum):
         updates.append((mparam_i, v))
     return updates
 
+def get_train_f(Ws, bs):
+    v, m, q, output, loss = get_model(Ws, bs, dropout=True)
+    updates = nesterov_updates(loss, Ws + bs, 1e-1, 0.9)
+    return theano.function([v, m, q], loss, updates=updates)
+
+def get_pred_f(Ws, bs):
+    v, m, q, output, loss = get_model(Ws, bs, dropout=False)
+    return theano.function([v, m, q], output)
+
 Ws, bs = get_parameters()
-m, v, loss = get_model(Ws, bs)
-updates = nesterov_updates(loss, Ws + bs, 1e-1, 0.9)
-loss_f = theano.function([m, v], loss, updates=updates)
+train_f = get_train_f(Ws, bs)
+pred_f = get_pred_f(Ws, bs)
 
 for iter in xrange(1000000):
-    print loss_f(M, V)
+    V, M, Q = build_matrices()
+    print train_f(V, M, Q)
 
     if (iter + 1) % 20 == 0:
         W = Ws[0].get_value()
@@ -129,5 +150,16 @@ for iter in xrange(1000000):
                 print '%.4f %30s %15.2f' % (cos(a, b), headers[j], split_x)
             print
 
-        # M_row, V_row = get_row(data['AAPL'])
-        # print M_row, V_row
+        V_row, M_row, Q_row = [x.reshape((1, K)) for x in get_row(data['AAPL'])]
+        V_row_recon = pred_f(V_row, M_row, Q_row).astype(theano.config.floatX)
+
+        print V_row[0,:10]
+        print V_row_recon[0,:10]
+        
+        cdfs = [[] for h in headers]
+        for i, split in enumerate(splits):
+            j, x_split = split
+            cdfs[j].append((x_split, V_row_recon[0][i]))
+
+        for j, header in enumerate(headers):
+            print header, sorted(cdfs[j])
