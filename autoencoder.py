a468bd0cda7ce200d90b3d301a9bfd5e3b7e2aff
Erik <Bernhardsson@Eriks-MacBook-Air-2.local>
2015-04-01 16:05:19 -0400

    more stuff

--- autoencoder.py
+++ autoencoder.py
@@ -5,19 +5,20 @@ import json
 import theano
 import theano.tensor as T
 from theano.tensor.nnet import sigmoid
+import pylab
 
 def floatX(x):
     return numpy.asarray(x, dtype=theano.config.floatX)
 
-
 srng = theano.tensor.shared_randomstreams.RandomStreams()
 
 def get_splits(headers, data, bins):
-    values = [[v[header] for v in data.values() if header in v] for header in headers]
+    values = [[v[header] for v in data if header in v] for header in headers]
 
     splits = []
     for j in xrange(len(headers)):
-        lo, hi = min(values[j]), max(values[j])
+        lo, hi = numpy.percentile(values[j], 1.0), numpy.percentile(values[j], 99.0)
+        print headers[j], lo, hi
         for bin in xrange(bins):
             x_split = lo + (bin + 1) * (hi - lo) * 1. / bins
             splits.append((j, x_split))
@@ -47,16 +48,16 @@ def get_row(headers, K, data_row, splits, headers_keep=None):
     return V_row, M_row, Q_row
 
 
-def build_matrices(headers, data, D, K, splits):
+def build_matrices(headers, data, D, K, splits, batch_size=100):
     V = numpy.zeros((D, K), dtype=theano.config.floatX)
     M = numpy.zeros((D, K), dtype=theano.config.floatX)
     Q = numpy.zeros((D, K), dtype=theano.config.floatX)
 
-    for i, key in enumerate(data.keys()):
+    for i, data_row in enumerate(random.sample(data, batch_size)):
         # How many header should we remove
         n_headers_keep = random.randint(0, len(headers))
         headers_keep = set(random.sample(headers, n_headers_keep))
-        V[i], M[i], Q[i] = get_row(headers, K, data[key], splits, headers_keep)
+        V[i], M[i], Q[i] = get_row(headers, K, data_row, splits, headers_keep)
 
     return V, M, Q
 
@@ -70,8 +71,8 @@ def W_values(n_in, n_out):
 
 def get_parameters(K):
     # Train an autoencoder to reconstruct the rows of the V matrices
-    n_hidden_layers = 3
-    n_hidden_units = 64
+    n_hidden_layers = 1
+    n_hidden_units = 32
     Ws, bs = [], []
     for l in xrange(n_hidden_layers + 1):
         n_in, n_out = n_hidden_units, n_hidden_units
@@ -93,7 +94,7 @@ def get_model(Ws, bs, dropout=False):
 
     # Set all missing/target values to 0.5
     keep_mask = (1-m) * (1-q)
-    h = v * keep_mask + 0.5 * (1 - keep_mask)
+    h = keep_mask * (v * 2 - 1) # Convert to +1, -1
     
     for l in xrange(len(Ws)):
         h = T.dot(h, Ws[l]) + bs[l]
@@ -112,22 +113,23 @@ def get_model(Ws, bs, dropout=False):
     return v, m, q, output, loss
 
 
-def nesterov_updates(loss, all_params, learn_rate, momentum):
+def nesterov_updates(loss, all_params, learn_rate, momentum, weight_decay):
     updates = []
     all_grads = T.grad(loss, all_params)
     for param_i, grad_i in zip(all_params, all_grads):
         # generate a momentum parameter
         mparam_i = theano.shared(numpy.array(param_i.get_value()*0.))
-        v = momentum * mparam_i - learn_rate * grad_i
-        w = param_i + momentum * v - learn_rate * grad_i
+        full_grad_i = grad_i + weight_decay * param_i
+        v = momentum * mparam_i - learn_rate * full_grad_i
+        w = param_i + momentum * v - learn_rate * full_grad_i
         updates.append((param_i, w))
         updates.append((mparam_i, v))
     return updates
 
 
 def get_train_f(Ws, bs):
-    v, m, q, output, loss = get_model(Ws, bs, dropout=True)
-    updates = nesterov_updates(loss, Ws + bs, 1e-1, 0.9)
+    v, m, q, output, loss = get_model(Ws, bs, dropout=False)
+    updates = nesterov_updates(loss, Ws + bs, 1e-1, 0.9, 1e-4)
     return theano.function([v, m, q], loss, updates=updates)
 
 
@@ -136,11 +138,13 @@ def get_pred_f(Ws, bs):
     return theano.function([v, m, q], output)
 
 
-def train(headers, data):
+def train(headers, data, plot_x=None, plot_y=None):
     D = len(data)
-    K = 1000 # Random splits
+    K = 200 # Random splits
     bins = K / len(headers)
     K = bins * len(headers)
+
+    print D, 'data points', K, 'random splits', bins, 'bins', K, 'features'
 
     splits = get_splits(headers, data, bins)
 
@@ -152,34 +156,35 @@ def train(headers, data):
         V, M, Q = build_matrices(headers, data, D, K, splits)
         print train_f(V, M, Q)
         
-        if (iter + 1) % 20 == 0:
-            W = Ws[0].get_value()
+        if (iter + 1) % 20 == 0 and header_plot_x and header_plot_y:
+            series = []
+            legends = []
+            cm = pylab.get_cmap('cool')
+            pylab.clf()
             
-            def cos(a, b):
-                p, q = W[a], W[b]
-                return numpy.dot(p, q) / math.sqrt(numpy.dot(p, p) * numpy.dot(q, q))
-        
-            for a in xrange(5):
-                bs = sorted(xrange(K), key=lambda b: cos(a, b), reverse=True)
-                for b in bs[:10]:
-                    j, split_x = splits[b]
-                    print '%.4f %30s %15.2f' % (cos(a, b), headers[j], split_x)
-                print
+            for j, x_split in splits:
+                if headers[j] == header_plot_x:
+                    data_row = {headers[j]: x_split}
+                    V, M, Q = [x.reshape((1, K)) for x in get_row(headers, K, data_row, splits)]
 
-            V_row, M_row, Q_row = [x.reshape((1, K)) for x in get_row(headers, K, data['AAPL'], splits)]
-            V_row_recon = pred_f(V_row, M_row, Q_row).astype(theano.config.floatX)
+                    P = pred_f(V, M, Q)
+                    
+                    xs = []
+                    ys = []
+                    for i, split in enumerate(splits):
+                        j2, x2_split = split
+                        if headers[j2] == header_plot_y:
+                            xs.append(x2_split)
+                            ys.append(P[0][i])
 
-            print V_row[0,:10]
-            print V_row_recon[0,:10]
-            
-            cdfs = [[] for h in headers]
-            for i, split in enumerate(splits):
-                j, x_split = split
-                cdfs[j].append((x_split, V_row_recon[0][i]))
+                    pylab.plot(xs, ys, color=cm(1.0 * len(legends) / (bins - 1)))
+                    legends += ['FICO %3d' % x_split]
 
-            for j, header in enumerate(headers):
-                print header, sorted(cdfs[j])
-    
+            # pylab.plot(*series)
+            # pylab.legend(legends)
+            pylab.savefig('interest_rates.png')
+
+
 if __name__ == '__main__':
     data = json.load(open('stock-data.json'))
 
